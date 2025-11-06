19d546cecffdacb9225ec058dc99c84d4ef9c5df
KARIᴴᵀᵀᴾ😌 <moneyheistofc@gmail.com>
2025-11-06 19:27:23 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 import Header from './components/Header';
 import Hero from './components/Hero';
+import About from './components/About';
 import Skills from './components/Skills';
 import Projects from './components/Projects';
 import Footer from './components/Footer';
@@ -10,6 +11,7 @@ export default function Home() {
       <Header />
       <div className="container">
         <Hero />
+        <About />
         <Skills />
         <Projects />
         <Footer />
