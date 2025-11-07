f069f9379b22bba0ce6f18a02944406baf08c8e0
KARIᴴᵀᵀᴾ😌 <moneyheistofc@gmail.com>
2025-11-07 08:35:05 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,9 +1,10 @@
-import Header from './components/Header';
-import Hero from './components/Hero';
-import PersonalInfo from './components/PersonalInfo';
-import Skills from './components/Skills';
-import Projects from './components/Projects';
-import Footer from './components/Footer';
+import Header from '../components/Header';
+import Hero from '../components/Hero';
+import PersonalInfo from '../components/PersonalInfo';
+import Skills from '../components/Skills';
+import Projects from '../components/Projects';
+import Footer from '../components/Footer';
+import Modals from '../components/Modal';
 
 export default function Home() {
   return (
@@ -16,6 +17,7 @@ export default function Home() {
         <Projects />
         <Footer />
       </div>
+      <Modals />
       
       {/* Font Awesome CDN */}
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
